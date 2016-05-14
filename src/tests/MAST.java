488d357a49bdf5f4c5a225d0acf301710f767501
Akknara <najeirh@gmail.com>
2016-05-14 18:20:27 +0300

    MAST somewhat complete (probably forgetting something)

--- src/tests/MAST.java
+++ src/tests/MAST.java
@@ -12,11 +12,13 @@ import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
 import customui.BorderButtonCustomUI;
+import customui.PanelCustomUI;
 import defaults.InterfaceTextDefaults;
 import defaults.TextLinkDefaults;
 import methods.Methods;
@@ -34,6 +36,7 @@ public class MAST extends AbstractTest {
 	JLabel question = new JLabel();
 
 	int currentQuestionNumber = 0;
+	int summ = 0;
 
 	public MAST(Methods methods, int width, int height, Test test) {
 		super(methods, width, height, test);
@@ -46,7 +49,6 @@ public class MAST extends AbstractTest {
 
 	@Override
 	public void showTest() {
-		// TODO
 		yesButton.setUI(new BorderButtonCustomUI(new Color(38, 166, 154)));
 		yesButton.setBorder(null);
 		yesButton.setOpaque(false);
@@ -56,7 +58,7 @@ public class MAST extends AbstractTest {
 		yesButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		yesButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO count points
+				summ += Integer.parseInt(n.item(currentQuestionNumber).getAttributes().getNamedItem("yes").getNodeValue());
 				if (currentQuestionNumber >= doc.getElementsByTagName("q").getLength() - 1) {
 					testTime = new Date().getTime() - testTime;
 					showResults();
@@ -75,7 +77,8 @@ public class MAST extends AbstractTest {
 		noButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		noButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO count points
+				summ += Integer
+						.parseInt(n.item(currentQuestionNumber).getAttributes().getNamedItem("no").getNodeValue());
 				if (currentQuestionNumber >= doc.getElementsByTagName("q").getLength() - 1) {
 					testTime = new Date().getTime() - testTime;
 					showResults();
@@ -144,8 +147,72 @@ public class MAST extends AbstractTest {
 
 	@Override
 	public void showResults() {
-		// TODO calculate and output results, do conclusion, high priority
 		showStandartResults();
+
+		JLabel leftCol = new JLabel();
+		JLabel rightCol = new JLabel();
+		JPanel conclusion = new JPanel();
+		String t;
+
+		NodeList d = doc.getElementsByTagName("d");
+
+		t = "<html><div style='font: 20pt Arial Narrow; color: rgb(144, 106, 96); text-align: right;'>"
+				+ d.item(0).getTextContent() + ": <br>" + "</div></html>";
+		leftCol.setText(t);
+
+		t = "<html><div style='font: bold 20pt Arial; color: rgb(38, 166, 154);'>" + summ + "<br>";
+		t += "</div></html>";
+		rightCol.setText(t);
+
+		t = "<html><div style='font: bold 20pt Arial; color: rgb(144, 106, 96); padding: 10px'>";
+		if (summ >= 0 && summ <= 4)
+			t += d.item(1).getTextContent().toUpperCase();
+		if (summ >= 5 && summ <= 7)
+			t += d.item(2).getTextContent().toUpperCase();
+		if (summ >7)
+			t += d.item(3).getTextContent().toUpperCase();
+		t += "</div></html>";
+
+		conclusion.add(new JLabel(t));
+		conclusion.setUI(new PanelCustomUI(true));
+
+		GridBagConstraints c = new GridBagConstraints();
+
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.weightx = 1.0;
+		c.weighty = 0.0;
+
+		c.insets = new Insets(10, 0, 0, 20);
+		c.anchor = GridBagConstraints.EAST;
+		c.gridwidth = 1;
+		// leftCol.setPreferredSize(new Dimension(300, 350));
+		leftCol.setVerticalAlignment(JLabel.TOP);
+		resultsPanel.add(leftCol, c);
+
+		c.gridx = 1;
+
+		c.anchor = GridBagConstraints.WEST;
+		c.insets = new Insets(10, 20, 0, 0);
+		c.gridwidth = 1;
+		// rightCol.setPreferredSize(new Dimension(300, 350));
+		rightCol.setVerticalAlignment(JLabel.TOP);
+		resultsPanel.add(rightCol, c);
+
+		c.anchor = GridBagConstraints.CENTER;
+		c.insets = new Insets(20, 0, 0, 0);
+		c.gridwidth = 2;
+		c.gridx = 0;
+		c.gridy = 2;
+		resultsPanel.add(conclusion, c);
+
+		this.revalidate();
+		this.repaint();
 	}
 
 	@Override
