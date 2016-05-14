57bd39d946d0faad5dba589f2de91ece5f67043d
Akknara <najeirh@gmail.com>
2016-05-14 21:20:21 +0300

    Spilberg somewhat complete

--- src/tests/Spilberg.java
+++ src/tests/Spilberg.java
@@ -26,6 +26,7 @@ import org.w3c.dom.NodeList;
 
 import customui.BorderButtonCustomUI;
 import customui.ButtonCustomUI;
+import customui.PanelCustomUI;
 import defaults.InterfaceTextDefaults;
 import defaults.TextLinkDefaults;
 import methods.Methods;
@@ -38,6 +39,9 @@ public class Spilberg extends AbstractTest {
 	Document doc = Utils.openXML(TextLinkDefaults.getInstance().getLink(TextLinkDefaults.Key.SPILBERG));
 
 	int currentQuestionNumber = 0;
+	int summ1 = 0;
+	int summ2 = 0;
+	int summAnxiety = 0;
 
 	JButton questionButton = new JButton();
 	ArrayList<JButton> answers = new ArrayList<JButton>();
@@ -54,11 +58,11 @@ public class Spilberg extends AbstractTest {
 
 	@Override
 	public void showTest() {
-		// TODO layout, middle priority
 		questionButton.setUI(new ButtonCustomUI(new Color(0, 168, 155)));
 		questionButton.setBorder(null);
 		questionButton.setOpaque(false);
-		questionButton.setPreferredSize(new Dimension(200, 35));
+		questionButton.setPreferredSize(new Dimension(200, 45));
+		questionButton.setMinimumSize(new Dimension(200, 45));
 
 		NodeList answersText = doc.getElementsByTagName("a");
 		AnswersListener l = new AnswersListener();
@@ -77,11 +81,22 @@ public class Spilberg extends AbstractTest {
 
 		nextButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				for (int i = 0; i < answers.size(); i++) {
+					if (((BorderButtonCustomUI) answers.get(i).getUI()).getBorderColor().equals(new Color(0, 168, 155))) {
+						switch (Integer.parseInt(doc.getElementsByTagName("q").item(currentQuestionNumber).getAttributes().getNamedItem("scale").getNodeValue())){
+						case 1:
+							summ1 += i+1;
+							break;
+						case 2:
+							summ2 += i+1;
+							break;
+						}
+					}
+				}				
 				if (currentQuestionNumber >= doc.getElementsByTagName("q").getLength() - 1) {
 					testTime = new Date().getTime() - testTime;
 					showResults();
 				} else {
-					// TODO count results, high priority
 					currentQuestionNumber++;
 					showQuestion();
 				}
@@ -90,30 +105,31 @@ public class Spilberg extends AbstractTest {
 
 		this.removeAll();
 		this.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
+		GridBagConstraints c = new GridBagConstraints();		
+		
 		c.anchor = GridBagConstraints.CENTER;
 		c.fill = GridBagConstraints.NONE;
 		c.gridheight = 1;
 		c.gridwidth = GridBagConstraints.REMAINDER;
 		c.gridx = 0;
-		c.gridy = 0;
-		c.insets = new Insets(40, 0, 40, 0);
 		c.ipadx = 0;
 		c.ipady = 0;
 		c.weightx = 0.0;
 		c.weighty = 0.0;
-
-		add(questionButton, c);
-
-		c.gridy = 1;
-		c.insets = new Insets(25, 0, 25, 0);
+		c.gridy = 0;
+		c.insets = new Insets(15, 0, 25, 0);
 
 		add(task, c);
+		
+		c.gridy = 1;
+		c.insets = new Insets(100, 0, 20, 0);
+
+		add(questionButton, c);
 
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridwidth = 1;
 		c.gridy = 2;
-		c.insets = new Insets(40, 20, 40, 20);
+		c.insets = new Insets(10, 20, 10, 20);
 		c.weightx = 0.1;
 
 		for (int i = 0; i < answersText.getLength(); i++) {
@@ -125,22 +141,22 @@ public class Spilberg extends AbstractTest {
 			Font f = ((BorderButtonCustomUI) b.getUI()).getFont();
 			FontMetrics fm = b.getFontMetrics(f);
 			int w = (int) fm.getStringBounds(b.getText(), b.getGraphics()).getWidth();
-			b.setPreferredSize(new Dimension(w + 40, 35));
+			b.setPreferredSize(new Dimension(w + 40, 45));
 			b.addActionListener(l);
 
 			answers.add(b);
 		}
 
 		// Myetod: "chje-rjez zhoo-puu"......
-		c.insets = new Insets(40, 60, 40, 15);
+		c.insets = new Insets(10, 60, 10, 15);
 		this.add(answers.get(0), c);
-		c.insets = new Insets(40, 15, 40, 15);
+		c.insets = new Insets(10, 15, 10, 15);
 		for (int i = 1; i < answers.size() - 1; i++) {
 			c.gridx = i;
 			this.add(answers.get(i), c);
 		}
 		c.gridx = 3;
-		c.insets = new Insets(40, 15, 40, 60);
+		c.insets = new Insets(10, 15, 10, 60);
 		this.add(answers.get(3), c);
 		// ...ne, nu esli inache nikak....
 
@@ -151,7 +167,7 @@ public class Spilberg extends AbstractTest {
 		c.anchor = GridBagConstraints.NORTH;
 		c.gridy = 3;
 		c.weighty = 0.1;
-		c.insets = new Insets(40, 0, 80, 0);
+		c.insets = new Insets(55, 0, 80, 0);
 
 		add(nextButton, c);
 
@@ -169,14 +185,77 @@ public class Spilberg extends AbstractTest {
 		Font f = new Font("Arial Narrow", Font.BOLD, 17);
 		FontMetrics fm = questionButton.getFontMetrics(f);
 		int w = (int) fm.getStringBounds(questionButton.getText(), questionButton.getGraphics()).getWidth();
-		questionButton.setPreferredSize(new Dimension(w + 40, 35));
+		questionButton.setPreferredSize(new Dimension(w + 40, 45));
 		questionButton.repaint();
 	}
 
 	@Override
 	public void showResults() {
-		// TODO calculate and show results, high priority
 		this.showStandartResults();
+		
+		JLabel leftCol = new JLabel();
+		JLabel rightCol = new JLabel();
+		
+		NodeList d = doc.getElementsByTagName("d");
+			
+		String t = "<html><div style='font: 20pt Arial Narrow; color: rgb(144, 106, 96); text-align: right;'>"
+				+ d.item(0).getTextContent() + ": <br>" + "</div></html>";
+		leftCol.setText(t);	
+
+		summAnxiety = summ1 - summ2 + 35;
+		
+		t = "<html><div style='font: bold 20pt Arial; color: rgb(38, 166, 154);'>"
+				+ summAnxiety + "<br>";
+		t += "</div></html>";
+		rightCol.setText(t);
+	
+		t = "<html><div style='font: bold 20pt Arial; color: rgb(144, 106, 96); padding: 10px'>";
+		if (summAnxiety <= 11) t += d.item(1).getTextContent().toUpperCase();
+		if (summAnxiety >= 12 && summAnxiety <= 30) t += d.item(2).getTextContent().toUpperCase();
+		if (summAnxiety >= 31 && summAnxiety <= 45) t += d.item(3).getTextContent().toUpperCase();
+		if (summAnxiety >= 46 ) t += d.item(4).getTextContent().toUpperCase();
+		t += "</div></html>";
+		JPanel conclusion = new JPanel();
+		conclusion.add(new JLabel(t));
+		conclusion.setUI(new PanelCustomUI(true));
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.anchor = GridBagConstraints.NORTHWEST;
+		c.fill = GridBagConstraints.NONE;
+		c.gridheight = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.weightx = 1.0;
+		c.weighty = 0.0;
+		
+		c.insets = new Insets(10, 0, 0, 20);
+		c.anchor = GridBagConstraints.EAST;
+		c.gridwidth = 1;
+		//leftCol.setPreferredSize(new Dimension(300, 350));
+		leftCol.setVerticalAlignment(JLabel.TOP);
+		resultsPanel.add(leftCol, c);
+
+		c.gridx = 1;
+		
+		c.anchor = GridBagConstraints.WEST;
+		c.insets = new Insets(10, 20, 0, 0);
+		c.gridwidth = 1;
+		//rightCol.setPreferredSize(new Dimension(300, 350));
+		rightCol.setVerticalAlignment(JLabel.TOP);
+		resultsPanel.add(rightCol, c);
+		
+		c.anchor = GridBagConstraints.CENTER;
+		c.insets = new Insets(20, 0, 0, 0);
+		c.gridwidth = 2;
+		c.gridx = 0;
+		c.gridy = 2;
+		resultsPanel.add(conclusion, c);
+		
+		this.revalidate();
+		this.repaint();
 	}
 
 	@Override
